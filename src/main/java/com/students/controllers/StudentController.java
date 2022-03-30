c5a0f7919ff9b064e6b277689d353dcb18686702
Aemiro Mekete <aemiromekete12@gmail.com>
2022-03-30 09:32:05 +0300

    Minor change on the response type

--- src/main/java/com/students/controllers/StudentController.java
+++ src/main/java/com/students/controllers/StudentController.java
@@ -7,6 +7,7 @@ import com.students.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -21,31 +22,29 @@ public class StudentController {
         this.studentService=studentService;
     }
     @PostMapping("create-student")
-    public StudentResponse createStudent(@RequestBody StudentDto studentDto) {
-        return studentService.saveStudent(studentDto);
+    public ResponseEntity<StudentResponse> createStudent(@RequestBody StudentDto studentDto) {
+        return ResponseEntity.ok(studentService.saveStudent(studentDto));
     }
     @PutMapping("update-student/{id}")
-    public StudentResponse updateStudent(@PathVariable("id") Long id, @RequestBody StudentDto studentDto) {
-        return studentService.updateStudent(id, studentDto);
+    public ResponseEntity<StudentResponse> updateStudent(@PathVariable("id") Long id, @RequestBody StudentDto studentDto) {
+        return ResponseEntity.ok(studentService.updateStudent(id, studentDto));
     }
     @GetMapping("get-students")
-    public List<StudentResponse> getStudents( @Param("keyword") String keyword) {
-        return studentService.getStudents(keyword);
+    public ResponseEntity<List<StudentResponse>> getStudents( @Param("keyword") String keyword) {
+        return ResponseEntity.ok(studentService.getStudents(keyword));
     }
     @GetMapping("get-student/{id}")
-    public StudentResponse getStudent(@PathVariable("id") Long id) {
+    public ResponseEntity<StudentResponse> getStudent(@PathVariable("id") Long id) {
         StudentResponse s=studentService.getStudent(id);
         System.out.println(s.toString());
         if(s!=null){
-            return s;
+            return ResponseEntity.ok(s);
         }
-        throw new ResponseStatusException(
-                HttpStatus.NOT_FOUND, "entity not found"
-        );
+       return ResponseEntity.notFound().build();
     }
     @DeleteMapping("delete-student/{id}")
-    public boolean deleteStudent(@PathVariable("id") Long id) {
+    public ResponseEntity<Boolean> deleteStudent(@PathVariable("id") Long id) {
          studentService.deleteStudent(id);
-         return true;
+         return ResponseEntity.ok(true);
     }
 }
